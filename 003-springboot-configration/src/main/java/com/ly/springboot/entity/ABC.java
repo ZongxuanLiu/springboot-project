d23080e62a5dbd8a89fecd1f6df2197e8253ee90
Zongxuan Liu <liuzongxuan@outlook.com>
2020-07-28 17:45:51 +0800

    7.28 提交

--- 003-springboot-configration/src/main/java/com/ly/springboot/entity/ABC.java
+++ 003-springboot-configration/src/main/java/com/ly/springboot/entity/ABC.java
@@ -1,9 +1,17 @@
 package com.ly.springboot.entity;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
-@Component
-@ConfigurationProperties(prefix = "abc")
+/**
+ *
+ * 使用@ConfigurationProperties注解出现警告问题，
+ * 在pom文件中添加spring-boot-configuration-processor的依赖
+ *
+ */
+@Component // 将此类交给spring容器进行管理
+@ConfigurationProperties(prefix = "abc") // prefix是配置文件中的前缀
 @Data
 public class ABC {
     private String name;
--- 003-springboot-configration/src/main/java/com/ly/springboot/web/IndexController.java
+++ 003-springboot-configration/src/main/java/com/ly/springboot/web/IndexController.java
@@ -1,15 +1,24 @@
 package com.ly.springboot.web;
 
 import com.ly.springboot.entity.ABC;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * @author liuying
+ */
 @Controller
 public class IndexController {
     @Value("${school.name}")
     private String schoolName;
     @Value("${websit}")
     private String websit;
+    /**
+     * 自定义配置文件映射到实体类
+     */
     @Autowired
     private ABC abc;
 
@@ -19,7 +28,8 @@ public class IndexController {
     }
 
     @RequestMapping(value = "abc")
-    public @ResponseBody String abc(){
+    public @ResponseBody
+    String abc(){
         return "abc.name:"+abc.getName();
     }
 }
